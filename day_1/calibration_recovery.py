d0a46c45f0c9bb82cc76d47c764089d437266169
Parv Rehan <z5421600@ad.unsw.edu.au>
2023-12-01 19:37:01 +1100

    Added input.txt & set-up blank .py file

---
+++ day_1/calibration_recovery.py
@@ -0,0 +1,10 @@
+# Given a line of letters & numbers, find the calibration value.
+# The calibration value can be found by combining the first digit and the last 
+# digit (in that order) to form a single two-digit number.
+
+# Your program must return the sum of all the calibration values.
+def main():
+    print("Hello World!")
+
+if __name__ == "__main__":
+    main()
