f5c4f46153e62ebde629c642713d40d841f4faab
Parv Rehan <z5421600@ad.unsw.edu.au>
2023-12-01 19:52:23 +1100

    Completed calibration_recovery (p1)

--- day_1/calibration_recovery.py
+++ day_1/calibration_recovery.py
@@ -3,8 +3,31 @@
 # digit (in that order) to form a single two-digit number.
 
 # Your program must return the sum of all the calibration values.
+def calc_calibration_value(line):
+    value = ""
+    for char in line:
+        if char.isnumeric():
+            value += char
+            break
+
+    for char in reversed(line):
+        if char.isnumeric():
+            value += char
+            break
+
+    return int(value)
+
 def main():
-    print("Hello World!")
+    input_file = open('input.txt', 'r')
+    data = input_file.readlines()
+
+    sum = 0
+    for line in data:
+        sum += calc_calibration_value(line)
+
+    print("The sum of all the calibration values is " + str(sum))
+
+    input_file.close()
 
 if __name__ == "__main__":
     main()
